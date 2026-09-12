94c0199a29247f2d70f2a6b5542421fcd20e9165
History Tracers <historytracers@gmail.com>
2026-09-12 17:16:44 -0300

    div_screens2: Embed Mexico City Museo image

--- app/src/main/java/com/historytracers/app/ui/components/LocalAssetImages.kt
+++ app/src/main/java/com/historytracers/app/ui/components/LocalAssetImages.kt
@@ -7,7 +7,9 @@ private val LOCAL_ASSET_IMAGES = mapOf(
     "https://www.historytracers.org/images/ESA/Planck_history_of_Universe.jpg"
         to "ESA/planck_history_of_universe.jpg",
     "https://www.historytracers.org/images/ESA/Planck_s_view_of_the_cosmic_microwave_background.jpg"
-        to "ESA/Planck_s_view_of_the_cosmic_microwave_background.jpg"
+        to "ESA/Planck_s_view_of_the_cosmic_microwave_background.jpg",
+    "https://www.historytracers.org/images/MexicoCityMuseo/HomoSapiens.jpg"
+        to "MexicoCityMuseo/HomoSapiens.jpg"
 )
 
 fun resolveImageSource(url: String): Any =
