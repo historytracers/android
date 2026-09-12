995ce8ff9321f40ebabbd1b13b0edb19f50fa82b
History Tracers <historytracers@gmail.com>
2026-09-12 16:41:05 -0300

    div_screens2: Embed another ESA image

--- app/src/main/java/com/historytracers/app/ui/components/LocalAssetImages.kt
+++ app/src/main/java/com/historytracers/app/ui/components/LocalAssetImages.kt
@@ -5,7 +5,9 @@ private const val ANDROID_ASSET_PREFIX = "file:///android_asset/"
 
 private val LOCAL_ASSET_IMAGES = mapOf(
     "https://www.historytracers.org/images/ESA/Planck_history_of_Universe.jpg"
-        to "ESA/planck_history_of_universe.jpg"
+        to "ESA/planck_history_of_universe.jpg",
+    "https://www.historytracers.org/images/ESA/Planck_s_view_of_the_cosmic_microwave_background.jpg"
+        to "ESA/Planck_s_view_of_the_cosmic_microwave_background.jpg"
 )
 
 fun resolveImageSource(url: String): Any =
