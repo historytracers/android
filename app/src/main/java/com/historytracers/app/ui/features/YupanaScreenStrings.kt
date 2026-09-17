6349cbf20805e3400aa36e2dafb64b93735cf166
History Tracers <historytracers@gmail.com>
2026-09-17 20:04:32 -0300

    indigenous: Cleanup Yupana

--- app/src/main/java/com/historytracers/app/ui/features/YupanaScreenStrings.kt
+++ app/src/main/java/com/historytracers/app/ui/features/YupanaScreenStrings.kt
@@ -4,25 +4,21 @@ package com.historytracers.app.ui.features
 import androidx.compose.runtime.staticCompositionLocalOf
 
 data class YupanaScreenStrings(
-    val quipu: String,
     val movingInYupana: String,
     val practicingMultiplicationYupana: String,
 )
 
 val EnYupanaScreenStrings = YupanaScreenStrings(
-    quipu = "Quipu",
     movingInYupana = "Moving in Yupana",
     practicingMultiplicationYupana = "Practicing Multiplication with Yupana",
 )
 
 val PtYupanaScreenStrings = YupanaScreenStrings(
-    quipu = "Quipu",
     movingInYupana = "Movendo no Yupana",
     practicingMultiplicationYupana = "Praticando Multiplica\u00e7\u00e3o com a Yupana",
 )
 
 val EsYupanaScreenStrings = YupanaScreenStrings(
-    quipu = "Quipu",
     movingInYupana = "Movi\u00e9ndose en Yupana",
     practicingMultiplicationYupana = "Practicando Multiplicaci\u00f3n con la Yupana",
 )
