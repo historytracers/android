8668aba448ec92d094cc3d85d124642f37c2d3b2
History Tracers <historytracers@gmail.com>
2026-09-18 20:46:49 -0300

    indigenous2: Update screen and sync repos

--- app/src/main/java/com/historytracers/app/ui/screens/IndexScreen.kt
+++ app/src/main/java/com/historytracers/app/ui/screens/IndexScreen.kt
@@ -66,7 +66,7 @@ private val whereAreWeFromSectionIds = listOf(
 )
 
 private val anotherWayToCountSectionIds = listOf(
-    "quipus", "practicing_with_quipus"
+    "quipus", "practicing_with_quipus", "mesoamerican_symbols"
 )
 
 private val runningAndGrowingSectionIds = listOf(
